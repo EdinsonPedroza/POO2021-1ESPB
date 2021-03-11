195e4af736eb7c4866c5fa72d208d3d53d326fb8
retro021 <include.tkd@gmail.com>
2021-03-10 19:36:56 -0500

    primer git

---
+++ POSGSOFT/cmake-build-debug/CMakeFiles/POSGSOFT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POSGSOFT.dir/main.cpp.obj"
+  "POSGSOFT.exe"
+  "POSGSOFT.exe.manifest"
+  "POSGSOFT.pdb"
+  "libPOSGSOFT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POSGSOFT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
