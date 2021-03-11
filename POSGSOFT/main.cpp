0b85ad3343c1f94162c1a8a092fd792fc7c39bf3
retro021 <include.tkd@gmail.com>
2021-03-10 21:31:51 -0500

    segundo commit

--- POSGSOFT/main.cpp
+++ POSGSOFT/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include<iostream>
+#include<list>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+//Hacer un enum del estado del acta {abierto, cerrado}
+class Jurado{
+    private:
+        string nombre;
+    public:
+        Jurado();
+        Jurado(string);
+};
+class Autor{
+    private:
+        string nombre;
+        int id;
+    public:
+        Autor();
+};
+
+class AsistenteMaestria{
+    private:
+        string nombre;
+        int id;
+    public:
+        AsistenteMaestria();
+        void   crearNuevaActa();
+
+};
+int main(){
+
+
 }
